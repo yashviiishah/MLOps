b709d0b44099f5119219cc27183533b5417d2d98
yashviiishah <yashvhs1334@gmail.com>
2025-04-15 01:34:32 +0530

    prep base mode added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.cnnClassifier import logger
 #from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+#from src.cnnClassifier.entity.config_entity import logger
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -12,3 +14,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
